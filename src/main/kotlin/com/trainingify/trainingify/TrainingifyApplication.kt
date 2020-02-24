c0edd1b2429c9118122d04b86f2d154941bc752c
Mikołaj Fedec <m.fedec11@gmail.com>
2020-02-24 21:00:55 +0100

    Initial commit

---
+++ src/main/kotlin/com/trainingify/trainingify/TrainingifyApplication.kt
@@ -0,0 +1,11 @@
+package com.trainingify.trainingify
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TrainingifyApplication
+
+fun main(args: Array<String>) {
+	runApplication<TrainingifyApplication>(*args)
+}
---
+++ src/test/kotlin/com/trainingify/trainingify/TrainingifyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.trainingify.trainingify
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TrainingifyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
